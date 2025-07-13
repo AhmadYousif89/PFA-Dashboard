787124b12e6bc925d3568014f8c24afe3b41c93b
Ahmad Yousif89 <ayob.4wrk89@gmail.com>
2025-07-13 14:56:39 +0300

    Add prettier and prettier tailwind plugin

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,7 @@
 export default function Home() {
-  return <div className=''></div>;
+  return (
+    <div className="bg-accent p-3 text-2xl text-gray-700 shadow">
+      <h1>Hello World</h1>
+    </div>
+  );
 }
