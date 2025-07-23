a75496884b1b5fc42c5ed387777070c345e43e62
Ahmad Yousif89 <ayob.4wrk89@gmail.com>
2025-07-23 19:56:08 +0300

    Refactor TransactionsSummary to use TransactionItem component

--- src/app/overview/_components/transactions-summary.tsx
+++ src/app/overview/_components/transactions-summary.tsx
@@ -1,7 +1,8 @@
-import Image from "next/image";
+import { Fragment } from "react";
 
 import DATA from "@/lib/data.json";
-import { Fragment } from "react";
+import { TransactionItem } from "@/components/transaction-item";
+import { Separator } from "@/components/ui/separator";
 
 export const TransactionsSummary = () => {
   const transactions = DATA.transactions.slice(0, 5);
@@ -9,40 +10,11 @@ export const TransactionsSummary = () => {
   return (
     <ul>
       {transactions.map((transaction, idx) => (
-        <Fragment key={transaction.name}>
+        <Fragment key={`transaction-${idx}`}>
           <li className="flex items-center justify-between">
-            <div className="flex items-center gap-4">
-              <Image
-                src={transaction.avatar}
-                alt={`${transaction.name}'s profile image`}
-                width={160}
-                height={160}
-                className="size-8 rounded-full"
-              />
-              <h3 className="text-14-bold">{transaction.name}</h3>
-            </div>
-            <div className="flex flex-col items-center gap-2">
-              <span
-                className={`text-14-bold ${transaction.amount > 0 ? "text-primary" : "text-foreground"}`}
-              >
-                {transaction.amount.toLocaleString("en-US", {
-                  style: "currency",
-                  currency: "USD",
-                  signDisplay: "always",
-                })}
-              </span>
-              <span className="text-12 text-muted-foreground">
-                {new Date(transaction.date).toLocaleString("en-GB", {
-                  day: "numeric",
-                  month: "short",
-                  year: "numeric",
-                })}
-              </span>
-            </div>
+            <TransactionItem data={transaction} />
           </li>
-          {idx < transactions.length - 1 && (
-            <hr className="border-muted my-5 h-px" />
-          )}
+          <Separator className="bg-muted my-5 last:hidden" />
         </Fragment>
       ))}
     </ul>
