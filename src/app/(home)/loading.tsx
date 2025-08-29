a4545168347408018879fe6e4bc68dc373d00f35
Ahmad Yousif89 <ayob.4wrk89@gmail.com>
2025-08-29 20:58:14 +0300

    Add LoadingDashboard component for loading state indication

---
+++ src/app/(home)/loading.tsx
@@ -0,0 +1,3 @@
+export default function LoadingDashboard() {
+  return <div className="flex flex-col items-center justify-center text-xl">Loading...</div>;
+}
--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -2,5 +2,5 @@ import { redirect } from "next/navigation";
 
 export default function Home() {
   redirect("/overview");
-  return <main className="bg-background grow"></main>;
+  return null;
 }
