ab496fcd6cf471aca16d6d94def9634743bc2e17
Ahmad Yousif89 <ayob.4wrk89@gmail.com>
2025-08-29 20:48:44 +0300

    Refactor type definitions in types.ts for improved clarity and structure; add DBDocument and BaseId interfaces, and enhance transaction-related types.

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,17 +1,72 @@
-import { categories, sortBy } from "@/components/data-table-toolbar";
+import { ObjectId } from "mongodb";
+import { themeColors, filterCategories, sortBy } from "./config";
 
-type SortByOrder = "asc" | "desc";
-type SortByKey = "date" | "name" | "amount";
-export type SortFormat = `${SortByKey}:${SortByOrder}`;
-export type SortTransactionBy = (typeof sortBy)[number];
+interface DBDocument {
+  _id?: string | ObjectId;
+}
+interface BaseId {
+  id: string;
+}
 
-export type TTransactionItem = {
+type DocumentType<T> = T & DBDocument;
+type BaseType<T> = T & BaseId;
+
+export type BalanceDocument = DocumentType<BalanceT>;
+export type PotDocument = DocumentType<PotT>;
+export type BillDocument = DocumentType<BillT>;
+export type BudgetDocument = DocumentType<BudgetT>;
+export type TransactionDocument = DocumentType<TransactionT>;
+
+export type Balance = BaseType<BalanceT>;
+export type Pot = BaseType<PotT>;
+export type Bill = BaseType<BillT>;
+export type Budget = BaseType<BudgetT>;
+export type Transaction = BaseType<TransactionT>;
+
+type BalanceT = {
+  current: number;
+  income: number;
+  expenses: number;
+};
+
+type PotT = {
+  name: string;
+  target: number;
+  total: number;
+  theme: ThemeColor;
+};
+
+type BillT = {
+  name: "Paid Bills" | "Total Upcoming" | "Due Soon";
+  amount: number;
+  theme: ThemeColor;
+};
+
+type BudgetT = {
+  category: TransactionCategory;
+  maximum: number;
+  theme: ThemeColor;
+};
+
+type TransactionT = {
   avatar: string;
   name: string;
-  category: string;
+  category: TransactionCategory;
   date: string;
   amount: number;
   recurring: boolean;
 };
 
-export type Category = (typeof categories)[number];
+type SortByOrder = "asc" | "desc";
+type SortByKey = "date" | "name" | "amount";
+export type SortFormat = `${SortByKey}:${SortByOrder}`;
+export type ThemeColorKey = keyof typeof themeColors;
+export type ThemeColor = (typeof themeColors)[ThemeColorKey];
+export type TransactionSortKey = (typeof sortBy)[number];
+export type TransactionCategory = (typeof filterCategories)[number];
+export type TransactionWithPaymentStatus = Transaction & {
+  paid?: boolean;
+  dueSoon?: boolean;
+  overdue?: boolean;
+  dueDay?: number;
+};
