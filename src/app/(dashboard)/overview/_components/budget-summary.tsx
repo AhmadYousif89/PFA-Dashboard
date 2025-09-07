129cb00f59e18a3db26027af29e8df7d42eaeaea
Ahmad Yousif89 <ayob.4wrk89@gmail.com>
2025-09-07 14:57:51 +0300

    Refactor BudgetsSummary component to use ScrollArea for improved layout and organization

--- src/app/(dashboard)/overview/_components/budget-summary.tsx
+++ src/app/(dashboard)/overview/_components/budget-summary.tsx
@@ -5,6 +5,7 @@ import {
 } from "@/app/(dashboard)/shared-data/budget";
 import { cn, formatCurrency } from "@/lib/utils";
 import { BudgetChart } from "@/components/budget-chart";
+import { ScrollArea } from "@/components/ui/scroll-area";
 
 export const BudgetsSummary = async () => {
   const budgets = await getBudgets();
@@ -18,30 +19,32 @@ export const BudgetsSummary = async () => {
   return (
     <div
       className={cn(
-        "flex min-h-75.5 flex-col gap-4 py-2",
+        "flex flex-col gap-4 py-2",
         "xl:@[22rem]/card-content:flex-row @[27rem]/card-content:flex-row",
       )}
     >
       <BudgetChart budgets={budgets} total={totalBudgetSpent} maximum={totalBudgetLimit} />
       {/* Legends */}
-      <ul
-        className={cn(
-          "grid grid-cols-2 gap-4",
-          "xl:@[22rem]/card-content:grid-cols-1 @[27rem]/card-content:grid-cols-1",
-        )}
-      >
-        {budgets.map((item, index) => (
-          <li key={index} className="flex items-center gap-4">
-            <span className="min-h-11 min-w-1 rounded" style={{ backgroundColor: item.theme }} />
-            <div className="flex flex-col gap-1">
-              <span className="text-muted-foreground text-xs whitespace-nowrap">
-                {item.category}
-              </span>
-              <span className="text-sm font-bold">{formatCurrency(item.maximum)}</span>
-            </div>
-          </li>
-        ))}
-      </ul>
+      <ScrollArea className="max-h-71.5">
+        <ul
+          className={cn(
+            "grid grid-cols-2 gap-4",
+            "xl:@[22rem]/card-content:grid-cols-1 @[27rem]/card-content:grid-cols-1",
+          )}
+        >
+          {budgets.map((item, index) => (
+            <li key={index} className="flex items-center gap-4">
+              <span className="min-h-11 min-w-1 rounded" style={{ backgroundColor: item.theme }} />
+              <div className="flex flex-col gap-1">
+                <span className="text-muted-foreground text-xs whitespace-nowrap">
+                  {item.category}
+                </span>
+                <span className="text-sm font-bold">{formatCurrency(item.maximum)}</span>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </ScrollArea>
     </div>
   );
 };
